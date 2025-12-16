745fb975b01f1d16b9a1110f1fe9941fca3f4d34
Mingqi Shi <smingqi@gmail.com>
2025-12-15 22:39:43 -0500

    add merge sort

--- src/sort_intro/insertion_sort.py
+++ src/sort_intro/insertion_sort.py
@@ -1,8 +1,18 @@
 def insertion_sort(arr):
-    return arr
+    for i in range(1, len(arr) ):
+        j = i
+        temp = arr[i]
+        while( j > 0 and temp > arr[j]):
+            arr[j] = arr[j - 1]
+            j -= 1
+        arr[j] = temp 
+        
+            
+   # return arr
 
 def main():
     arr = [1,2,6,2,3,9,7,8,11]
     insertion_sort(arr)
+    print(arr)
 
 main()
---
+++ src/sort_intro/merge_sort.py
@@ -0,0 +1,49 @@
+def merge_sort(arr):
+    
+    if len(arr) < 2:
+        return arr
+    
+    mid = len(arr)//2
+    
+    left_arr = arr[: mid]
+    right_arr = arr[mid: ]
+    
+    left_arr = merge_sort(left_arr)
+    right_arr = merge_sort(right_arr)
+    arr = merge(left_arr, right_arr)
+    
+    return arr
+    
+
+    
+    
+def merge(left_arr, right_arr):
+    arr = []
+    i = j = 0
+    while( i < len(left_arr) and j < len(right_arr) ):
+        if left_arr[i] <= right_arr[j]:
+            arr.append(left_arr[i])
+            i += 1
+        else:
+            arr.append(right_arr[j])
+            j += 1
+    
+    while( i < len(left_arr) ):
+        arr.append(left_arr[i])
+        i += 1
+        
+    while( j < len(right_arr) ):
+        arr.append( right_arr[j])
+        j += 1    
+    
+    return arr
+    
+    
+def main():
+    arr = [1,6,5,2,7,3,4,8,0]
+    arr= merge_sort(arr)
+    print(arr)
+    print(len(arr)//2)
+    #
+    
+main()
